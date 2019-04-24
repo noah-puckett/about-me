3b8b48c2ac75dbd9d6be95e0ed439f61929a3793
Noah Puckett <noah.ingram.puckett@gmail.com>
2019-04-23 17:00:56 -0700

    renamed variables to be more accessible for screen-readers, updated and added CSS file

---
+++ src/index.js
@@ -0,0 +1,7 @@
+const cuddle = document.getElementById('cuddle');
+const say = document.getElementById('say');
+const do1 = document.getElementById('do1');
+const hiss = document.getElementById('hiss');
+const hydrate = document.getElementById('hydrate');
+//DO I NEED A BUTTON.GETELEMENTBYID HERE???
+const htmlParagraph = document.getElementById('html-paragraph');
