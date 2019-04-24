0a19b211170c3d71b836924f3243fa19c6915df8
Noah Puckett <noah.ingram.puckett@gmail.com>
2019-04-23 17:43:37 -0700

    fixed linter issues in all files, console logged variables in callback function

--- src/index.js
+++ src/index.js
@@ -5,10 +5,25 @@ const hiss = document.getElementById('hiss');
 const hydrate = document.getElementById('hydrate');
 //DO I NEED A BUTTON.GETELEMENTBYID HERE???LMAO YES I DO 
 
-const submitButton = document.getElementById('submit-button')
+const submitButton = document.getElementById('submit-button');
 const htmlParagraph = document.getElementById('html-paragraph');
 
-submitButton.addEventListener('click', () => {
-    alert('hOLY SHIT YOU DID IT');
+submitButton.addEventListener('click', (event) => {
+    event.preventDefault();
+    
+    const cuddleValue = cuddle.value;
+    const sayValue = say.value;
+    const do1Value = do1.value;
+    const hissValue = hiss.value;
+    const hydrateValue = hydrate.value;
+
+    console.log(cuddleValue, 'cuddle');
+    console.log(sayValue, 'sayvalue');
+    console.log(do1Value, 'dovalue');
+    console.log(hissValue, 'hissvalue');
+    console.log(hydrateValue, 'hydratevalue');
+
 });
+
+
 
