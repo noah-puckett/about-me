a718689ea3b9e39606d2a351633a03e9cdbe1545
Noah Puckett <noah.ingram.puckett@gmail.com>
2019-04-23 17:14:58 -0700

    added event listener to submit button in js file

--- src/index.js
+++ src/index.js
@@ -3,5 +3,12 @@ const say = document.getElementById('say');
 const do1 = document.getElementById('do1');
 const hiss = document.getElementById('hiss');
 const hydrate = document.getElementById('hydrate');
-//DO I NEED A BUTTON.GETELEMENTBYID HERE???
+//DO I NEED A BUTTON.GETELEMENTBYID HERE???LMAO YES I DO 
+
+const submitButton = document.getElementById('submit-button')
 const htmlParagraph = document.getElementById('html-paragraph');
+
+submitButton.addEventListener('click', () => {
+    alert('hOLY SHIT YOU DID IT');
+});
+
